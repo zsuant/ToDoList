fc27fe9b15169776f339bf49e7613e163b0a187b
zsuant <lsg001025@gmail.com>
2023-02-07 10:58:49 +0900

    step 1

---
+++ ToDoList/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  ToDoList
+//
+//  Created by 이수겸 on 2023/02/07.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -13,7 +13,7 @@ struct ToDoListApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
