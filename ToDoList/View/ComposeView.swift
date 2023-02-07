8471fb2c063ab1870c5bd319b382e1501a036174
zsuant <lsg001025@gmail.com>
2023-02-07 19:01:38 +0900

    Step5

---
+++ ToDoList/View/ComposeView.swift
@@ -0,0 +1,51 @@
+//
+//  ComposeView.swift
+//  ToDoList
+//
+//  Created by 이수겸 on 2023/02/07.
+//
+
+import SwiftUI
+
+struct ComposeView: View {
+    @EnvironmentObject var store: ToDoStore
+    
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var content: String = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextEditor(text: $content)
+                    .padding()
+            }
+            .navigationTitle("New ToDo")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("취소")
+                    }
+                }
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button {
+                        store.insert(toDoText: content)
+                        dismiss()
+                    } label: {
+                        Text("저장")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ComposeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ComposeView()
+            .environmentObject(ToDoStore())
+    }
+}
